bf33eec078fe54a4a5e10fbf4a6d78d47371ccba
nadine-shi <nadine@afterlogic.com>
2016-09-21 09:44:59 +0300

    Core - copyright was added

--- module.php
+++ module.php
@@ -1,4 +1,20 @@
 <?php
+/*
+ * @copyright Copyright (c) 2016, Afterlogic Corp.
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
 
 class CoreModule extends AApiModule
 {
@@ -1230,7 +1246,7 @@ class CoreModule extends AApiModule
 	 */
 	public function GetUserList($Offset = 0, $Limit = 0, $OrderBy = 'Name', $OrderType = \ESortOrder::ASC, $Search = '')
 	{
-//		\CApi::checkUserRoleIsAtLeast(\EUserRole::TenantAdmin);
+		\CApi::checkUserRoleIsAtLeast(\EUserRole::TenantAdmin);
 		
 		$aResults = $this->oApiUsersManager->getUserList($Offset, $Limit, $OrderBy, $OrderType, $Search);
 		$aUsers = array();
@@ -1265,7 +1281,7 @@ class CoreModule extends AApiModule
 			$oUser->IdTenant = $TenantId;
 			$oUser->Role = $Role;
 
-			if ($this->oApiUsersManager->createUser($oUser));
+			if ($this->oApiUsersManager->createUser($oUser))
 			{
 				return $oUser->iId;
 			}
