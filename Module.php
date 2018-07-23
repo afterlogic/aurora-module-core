bfd52dae7087891d4456cdb364c9acc0283dc7b0
nadine-shi <nadine@afterlogic.com>
2018-07-23 12:50:38 +0300

    BUG - the language for the entire system is displayed incorrectly in Common tab of AdminPanel

--- Module.php
+++ Module.php
@@ -1197,6 +1197,7 @@ For instructions, please refer to this section of documentation and our
 				'AdminLogin' => $oSettings->GetConf('AdminLogin'),
 				'AdminHasPassword' => !empty($sAdminPassword),
 				'AdminLanguage' => $oSettings->GetConf('AdminLanguage'),
+				'CommonLanguage' => $this->getConfig('Language'),
 				'SaltNotEmpty' => file_exists(\Aurora\System\Api::DataPath() . '/salt.php') && strlen(@file_get_contents(\Aurora\System\Api::DataPath() . '/salt.php')),
 				'EnableLogging' => $oSettings->GetConf('EnableLogging'),
 				'EnableEventLogging' => $oSettings->GetConf('EnableEventLogging'),
