bc9757ece3fb5c80b29a16d3823e6691bff509f3
sash04ek <a.ovcharov@afterlogic.com>
2022-04-19 16:01:50 +0300

    added check indexes if they already exist

--- Migrations/2022_04_05_183315_create_core_fulltext_indexes.php
+++ Migrations/2022_04_05_183315_create_core_fulltext_indexes.php
@@ -13,25 +13,37 @@ class CreateCoreFulltextIndexes extends Migration
      */
     public function up()
     {
-        Capsule::connection()->beginTransaction();
-
         $prefix = Capsule::connection()->getTablePrefix();
-        Capsule::schema()->table('core_groups', function (Blueprint $table)
-        {
-            $table->index(['TenantId']);
-        });
-        Capsule::schema()->table('core_auth_tokens', function (Blueprint $table)
-        {
-            $table->index(['UserId']);
-        });
-        Capsule::schema()->table('core_min_hashes', function (Blueprint $table)
-        {
-            $table->index(['UserId']);
-        });
-        Capsule::statement("CREATE FULLTEXT INDEX ccore_tenants_name_index ON {$prefix}core_tenants (Name)");
-        Capsule::statement("CREATE FULLTEXT INDEX ccore_groups_name_index ON {$prefix}core_groups (Name)");
+        $sm = Capsule::connection()->getDoctrineSchemaManager();
 
-        Capsule::connection()->commit();
+        Capsule::schema()->table('core_groups', function (Blueprint $table) use ($prefix, $sm)
+        {
+            $doctrineTable = $sm->listTableDetails($prefix . 'core_groups');
+            if (!$doctrineTable->hasIndex('core_groups_tenantid_index')) {
+                $table->index(['TenantId']);
+            }
+            if (!$doctrineTable->hasIndex('ccore_groups_name_index')) {
+                Capsule::statement("CREATE FULLTEXT INDEX ccore_groups_name_index ON {$prefix}core_groups (Name)");
+            }
+        });
+        Capsule::schema()->table('core_auth_tokens', function (Blueprint $table) use ($prefix, $sm)
+        {
+            $doctrineTable = $sm->listTableDetails($prefix . 'core_auth_tokens');
+            if (!$doctrineTable->hasIndex('core_auth_tokens_userid_index')) {
+                $table->index(['UserId']);
+            }
+        });
+        Capsule::schema()->table('core_min_hashes', function (Blueprint $table) use ($prefix, $sm)
+        {
+            $doctrineTable = $sm->listTableDetails($prefix . 'core_min_hashes');
+            if (!$doctrineTable->hasIndex('core_min_hashes_userid_index')) {
+                $table->index(['UserId']);
+            }
+        });
+        $doctrineTable = $sm->listTableDetails($prefix . 'core_tenants');
+        if (!$doctrineTable->hasIndex('ccore_tenants_name_index')) {
+            Capsule::statement("CREATE FULLTEXT INDEX ccore_tenants_name_index ON {$prefix}core_tenants (Name)");
+        }
     }
 
     /**
