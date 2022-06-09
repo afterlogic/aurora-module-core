be8535f38a739f969c05baeecc408df2f764c96a
sash04ek <a.ovcharov@afterlogic.com>
2022-06-09 14:50:30 +0300

    fixed BUG - remove the core_user_groups_legacy table

---
+++ Migrations/2022_06_09_143701_delete_user_groups_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+class DeleteUserGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Capsule::schema()->dropIfExists('core_user_groups_legacy');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {        
+        Capsule::schema()->create('core_user_groups_legacy', function (Blueprint $table) {
+            $table->id('Id');
+            $table->string('UrlIdentifier')->default('');
+            $table->integer('IdTenant')->default(0);
+            $table->json('Properties')->nullable();
+            $table->timestamp(\Aurora\System\Classes\Model::CREATED_AT)->nullable();
+            $table->timestamp(\Aurora\System\Classes\Model::UPDATED_AT)->nullable();
+        });
+    }
+}
