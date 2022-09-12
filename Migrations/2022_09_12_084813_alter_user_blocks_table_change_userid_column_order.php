f0d6b57211fee451652663c71d2ee417fd1a7e28
sash04ek <a.ovcharov@afterlogic.com>
2022-09-12 09:35:51 +0300

    Correct the order of fields in the core_user_blocks DB table

---
+++ Migrations/2022_09_12_084813_alter_user_blocks_table_change_userid_column_order.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+class AlterUserBlocksTableChangeUseridColumnOrder extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $prefix = Capsule::connection()->getTablePrefix();
+        Capsule::statement("ALTER TABLE {$prefix}core_user_blocks MODIFY UserId int(11) AFTER Id");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $prefix = Capsule::connection()->getTablePrefix();
+        Capsule::statement("ALTER TABLE {$prefix}core_user_blocks MODIFY UserId int(11) AFTER UpdatedAt");
+    }
+}
