50581e3f80905119e684d111dfabbba32692d3ef
sash04ek <a.ovcharov@afterlogic.com>
2023-04-28 13:22:23 +0300

    fixed - BUG - can't update DB due to invalid value of TimeFormat field

--- Migrations/2023_04_25_154456_alter_users_table_update_timeformat_column.php
+++ Migrations/2023_04_25_154456_alter_users_table_update_timeformat_column.php
@@ -1,5 +1,7 @@
 <?php
 
+use Aurora\Modules\Core\Models\User;
+use Aurora\Modules\Core\Module as CoreModule;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Capsule\Manager as Capsule;
@@ -13,6 +15,11 @@ class AlterUsersTableUpdateTimeformatColumn extends Migration
      */
     public function up()
     {
+        User::where('TimeFormat', null)
+            ->update([
+                'TimeFormat' => CoreModule::getInstance()->getModuleSettings()->TimeFormat
+        ]);
+
         Capsule::schema()->table('core_users', function (Blueprint $table) {
             $table->integer('TimeFormat')->nullable(false)->change();
         });
